702302b66b0600659e813ad3ae2dbad7963f7215
LuisBola12 <luibv1202@gmail.com>
2023-04-14 10:03:01 -0600

    Implementacion de Regresion con soporte vectorial

---
+++ regresion_arboles_decision/regresion_arboles_decision.py
@@ -0,0 +1,50 @@
+
+
+
+
+
+
+#Arbol de decision para regresion
+
+import numpy as npy
+import matplotlib.pyplot as pyplot
+import pandas as pds 
+
+#importar el dataset
+dataset = pds.read_csv('Position_Salaries.csv')
+
+x = dataset.iloc[:,1:2].values
+#variable a predecir
+y = dataset.iloc[:,2].values
+
+#Al tener solo 10 datos, y buscar datos precisos no es bueno dividir el dataset
+""""from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(
+    x,y,test_size = 0.2,random_state = 0)"""
+#escalado de variables
+"""from sklearn.preprocessing import StandardScaler
+std_scl_in = StandardScaler()
+var_in_train = std_scl_in.fit_transform(var_in_train)
+var_in_test = std_scl_in.transform(var_in_test)"""
+
+#Ajustar la regresion con el dataset
+from sklearn.tree import DecisionTreeRegressor
+regression = DecisionTreeRegressor(random_state = 0)
+regression.fit(x,y)
+
+#Prediccion con arboles de decision
+predictions = regression.predict([[6.5]])
+
+
+#visualizacion de los resultados 
+x_grid = npy.arange(min(x),max(x),0.1)
+x_grid = x_grid.reshape(len(x_grid),1)
+pyplot.scatter(x,y,color = "red")
+pyplot.plot(x,regression.predict(x),color = "blue")
+pyplot.title("Modelo Regresion con árboles de decision")
+pyplot.xlabel("Titulo eje X")
+pyplot.ylabel("Titulo eje Y")
+pyplot.show()
+
+
+
